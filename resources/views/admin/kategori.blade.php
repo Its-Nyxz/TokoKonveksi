2fd63865c4d43eb71a3e26ffe1000152c667115c
Lenovo <orvinoray@gmail.com>
2026-01-24 21:59:11 +0700

    perbaikan tombol aksi dan penambahan sweet alert di tombol hapus

--- resources/views/admin/kategori.blade.php
+++ resources/views/admin/kategori.blade.php
@@ -10,7 +10,7 @@
             <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between bg-coklat">
                     <h6 class="m-0 font-weight-bold text-white">Data Kategori</h6>
-                </div> 
+                </div>
                 <div class="card-body">
                     <table class="table table-bordered" id="table">
                         <thead>
@@ -27,11 +27,13 @@
                                     <td><?php echo $nomor; ?></td>
                                     <td>{{ $data->namakategori }}</td>
                                     <td>
-                                        <a href="{{ url('admin/ubahkategori/' . $data->idkategori) }}"
-                                            class="btn btn-primary btn-sm">Ubah</a>
-                                        <a href="{{ url('admin/hapuskategori/' . $data->idkategori) }}"
-                                            class="btn btn-danger btn-sm"
-                                            onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data ?')">Hapus</a>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <a href="{{ url('admin/ubahkategori/' . $data->idkategori) }}"
+                                                class="btn btn-primary btn-sm"><i class="fa-solid fa-pen-to-square"></i></a>
+                                            <a href="{{ url('admin/hapuskategori/' . $data->idkategori) }}"
+                                                class="btn btn-danger btn-sm btn-delete"><i
+                                                    class="fa-solid fa-trash"></i></a>
+                                        </div>
                                     </td>
                                     </td>
 
@@ -43,5 +45,34 @@
                 </div>
             </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        <script>
+            $(document).ready(function() {
+                // Gunakan $(document).on agar tombol di halaman pagination tetap berfungsi
+                $(document).on('click', '.btn-delete', function(e) {
+                    e.preventDefault();
+
+                    var getLink = $(this).attr('href');
+
+                    Swal.fire({
+                        title: "Apakah Anda Yakin?",
+                        text: "Data kategori ini akan dihapus secara permanen!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Ya, Hapus!",
+                        cancelButtonText: "Batal"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = getLink;
+                        }
+                    });
+                });
+            });
+        </script>
     </div>
 @endsection
