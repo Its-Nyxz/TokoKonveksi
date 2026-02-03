1b509d378727944a5a4bd29db7526fa91812a79e
Lenovo <orvinoray@gmail.com>
2026-02-03 12:19:44 +0700

    penambahan konfirmasi penghapusan pengguna dengan SweetAlert pada tombol hapus

--- resources/views/admin/pengguna.blade.php
+++ resources/views/admin/pengguna.blade.php
@@ -29,8 +29,8 @@
                                     <td>{{ $pecah->telepon }}</td>
                                     <td>{{ $pecah->alamat }}</td>
                                     <td>
-                                        <a href="{{ url('admin/hapuspengguna/' . $pecah->id) }}" class="btn btn-danger"
-                                            onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data ?')">Hapus</a>
+                                        <a href="{{ url('admin/hapuspengguna/' . $pecah->id) }}"
+                                            class="btn btn-danger btn-delete">Hapus</a>
                                     </td>
                                 </tr>
                                 <?php $nomor++; ?>
@@ -40,5 +40,34 @@
                 </div>
             </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        <script>
+            $(document).ready(function() {
+                // Gunakan $(document).on agar tombol di halaman pagination tetap berfungsi
+                $(document).on('click', '.btn-delete', function(e) {
+                    e.preventDefault();
+
+                    var getLink = $(this).attr('href');
+
+                    Swal.fire({
+                        title: "Apakah Anda Yakin?",
+                        text: "Data pengguna ini akan dihapus secara permanen!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Ya, Hapus!",
+                        cancelButtonText: "Batal"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = getLink;
+                        }
+                    });
+                });
+            });
+        </script>
     </div>
 @endsection
